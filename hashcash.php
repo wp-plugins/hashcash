ac59572a8932157953a346f273f5fac154f19a8d
pkaroukin <pkaroukin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-08 20:52:24 +0000

    Preparing for 1.0.12 release
    git-svn-id: https://plugins.svn.wordpress.org/hashcash/trunk@1108349 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hashcash.php
+++ hashcash.php
@@ -13,7 +13,7 @@
  * Plugin URI: http://wordpress.org/plugins/hashcash/
  * Description: Hashcash.IO Integration
  * Author: Pavel A. Karoukin, haboes
- * Version: 1.0.11
+ * Version: 1.0.12
  * GitHub Plugin URI: https://github.com/hashcash/wordpress-hashcash/
 */
 
